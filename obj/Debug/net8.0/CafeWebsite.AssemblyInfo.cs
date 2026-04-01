80e4ba4cbb6662fbe65ab7bdda5ce19b7e10eec8
AMNVLFR <mrniceguy1234337@gmail.com>
2026-04-01 22:34:36 +0800

    Initial commit

--- obj/Debug/net8.0/CafeWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/CafeWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CafeWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20687bc82b50ba02169c1d30ea5108aafb3b5ab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f7bac6aacae77996ee0f6d753e5fbeca1c5b5b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CafeWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CafeWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
